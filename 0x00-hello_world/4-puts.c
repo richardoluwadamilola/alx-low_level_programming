c4322b666ae58313385ccc317c337823822a3fa1
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-06-30 04:11:07 -0700

    hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,10 @@
 #include <stdio.h>
 
-/**
- *
- *  * main - Prints quote
- *
- *   *
- *
- *    * Return: zero on success
- *
- *     *
- *
- *      */
 
 int main(void)
-
 {
 
-	puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 
 }
