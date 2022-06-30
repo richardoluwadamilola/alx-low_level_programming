ff0f4c1f66f6b51963e34b8caa79e8443d0e00e7
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-06-30 04:16:19 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,16 +4,11 @@
  *
  * * main - prints the size of various types
  *
- * * Return: 0 if exited properly, non-zero otherwise
+ * * Return: zero if exited properly, non-zero otherwise
  *
  * */
-
-
-
 int main(void)
 {
-
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-
 }
