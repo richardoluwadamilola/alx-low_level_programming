ef7cc09ab03cb45d7d30fdbdfdec117a1b5f3522
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-06-30 06:35:32 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+/*
+ *
+ * main - Prints quote to the standard error
+ *
+ * Description: "the program's descriptioni"
+ *
+ * Return: zero on success
+ *
+ *
+ */
+
+int main(void)
+
+{
+
+		write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+
+			return (1);
+
+}
