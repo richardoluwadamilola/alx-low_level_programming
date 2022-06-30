ed5cd5895eb29bec32d283edd5bfe04c7024ea46
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-06-30 04:36:54 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,11 +8,10 @@
  *
  * Return: zero on success
  *
- *    
  *
  */
 int main(void)
 {
- printf("with proper grammar, but the outcome is a piece of art,\n");
- return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
