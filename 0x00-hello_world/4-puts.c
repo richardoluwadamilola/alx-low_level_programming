47c6fa9f6bb1d40f098b62d6fc186df50471d2cf
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-06-30 05:33:16 -0700

    size

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 /*
+ * main - entry point
  *
- * main - print quote
- *
- * Return: zero on success
- *
+ * Return: 0 after printing the function
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
