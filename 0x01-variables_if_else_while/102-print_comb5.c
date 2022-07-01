9981cbd7e111807f582ea653e726b09781283e71
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-07-01 06:27:23 -0700

    combs5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - writes combinations of 00-99
+ * @void: empty parameter list for main.
+ *
+ * Description: writes all unique combinations of 2 digit numbers
+ *
+ * Returns: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = 0; j <= 99; j++)
+		{
+			if (i < j)
+			{
+				putchar((i/10) + '0');
+				putchar((1 % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
