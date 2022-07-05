214e7246ff24d2e1496f8eb655eca8a82c14c316
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-07-05 07:50:03 -0700

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -29,13 +29,11 @@ void print_times_table(int n)
 					_putchar(44);
 					_putchar(32);
 					_putchar(32);
-					_putchar(32);
 					_putchar(c + '0');
 				}
 				else if (c >= 10 && c < 100)
 				{
 					_putchar(44);
-					_putchar(32);
 					_putchar(32);
 					_putchar((c / 10) + '0');
 					_putchar((c % 10) + '0');
