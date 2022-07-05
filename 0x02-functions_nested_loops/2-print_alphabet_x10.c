e6c3c6b4f1071248776633b795bb5f5fcf84b210
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-07-05 04:23:51 -0700

    print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercase alphabet
+ *
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = '0'; i <='9'; i++)
+	{
+		print_alphabet();
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,6 +3,8 @@
 /**
  * _putchar - writes the character c to stdout
  * print_alphabet - prints alphabet and returns a new line
+ * print_alphabet_x10 - print alphabet 10 times
  */
 int _putchar(char);
 void print_alphabet(void);
+void print_alphabet_x10(void);
