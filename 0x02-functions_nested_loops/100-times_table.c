5ed329d914df52e69ee1a1ce8a8fb4b4b2ad035b
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-07-05 07:44:07 -0700

    n times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+ * print_times_table - print times tables up to input
+ *
+ * Description: writes times table up to input not exceediny 15
+ *
+ * @n: size of times table
+ *
+ * Return: Nothing
+ */
+void print_times_table(int n)
+{
+	int a, b, c;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				c = b * a;
+				if (b == 0)
+				{
+					_putchar(c + '0');
+				}
+				else if (c < 10 && b != o)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+					_putchar(32);
+					_putchar(c + '0');
+				}
+				else if (c >= 10 && c < 100)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+					_putchar((c / 10) + '0');
+					_putchar((c % 10) + '0');
+				}
+				else if (c >= 100)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar((c / 100) + '0');
+					_putchar(((c / 10) % 10) + '0');
+					_putchar((c % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
+
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,6 +13,7 @@
  * times_table - prints times table up to 9
  * add - add two ints, return sum
  * print_to_98 - prints all natural numbers to 98
+ * print_times_table - prints table table up to 15
  */
 int _putchar(char);
 void print_alphabet(void);
@@ -26,3 +27,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
