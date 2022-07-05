3d7bd85d08d2ee44e8840ef405d334e37cc093f6
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-07-05 08:05:46 -0700

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print sum of all multiples of 3 and 5 up to 1024
+ *
+ * @void: empty parameter list for main
+ *
+ * Description: prints the sum of all multiples of 3 and 5
+ * up to but not including 1024
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int sum, i;
+
+	sum = 0;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
