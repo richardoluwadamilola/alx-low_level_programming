0a5d547640209fdde6e8e944d0f48c3d82ea246d
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-07-05 05:11:17 -0700

    print sign

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,9 +6,11 @@
  * print_alphabet_x10 - print alphabet 10 times
  * _islower - return 1 if c is lowercase, and 0 otherwise
  * _isalpha - return 1 if c is a letter, upper or lowercase, 0 otherwise
+ * print_sign - return 1 for +, 0 for 0, -1  for -
  */
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
+int print_sign(int n);
