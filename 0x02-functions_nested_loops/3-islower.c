cd2bc8c9dcbe96fae3987ef04c014576333dd7a1
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-07-05 04:47:52 -0700

    lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,14 +3,11 @@
 /**
  * _islower - returns 1 if c is lowercase
  *
- * @c: character to be checked if lower case, type int
+ * @c: character to be checked if lower case
  *
  * Return: 1 if lowercase, 0 otherwise
  */
 int _islower(int c)
 {
-	if (c <= 97 && c <= 122)
-		return (1);
-	else
-		return (0);
+	return (c >= 'a' && c <= 'z');
 }
