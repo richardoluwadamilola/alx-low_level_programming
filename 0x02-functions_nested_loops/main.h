fbe93d7265c26c89911e24cc780560cfc0bf61af
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-07-05 05:19:11 -0700

    absolute value

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,6 +7,7 @@
  * _islower - return 1 if c is lowercase, and 0 otherwise
  * _isalpha - return 1 if c is a letter, upper or lowercase, 0 otherwise
  * print_sign - return 1 for +, 0 for 0, -1  for -
+ * _abs - computes absolute value of integer
  */
 int _putchar(char);
 void print_alphabet(void);
@@ -14,3 +15,4 @@ void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int);
