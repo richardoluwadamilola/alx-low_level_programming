18fc9cab85241d57e1a411ce811c61d00611c869
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-07-05 06:54:34 -0700

    n to 98

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,6 +12,7 @@
  * jack_bauer - prints all the minutes of his day
  * times_table - prints times table up to 9
  * add - add two ints, return sum
+ * print_to_98 - prints all natural numbers to 98
  */
 int _putchar(char);
 void print_alphabet(void);
@@ -24,3 +25,4 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
+void print_to_98(int n);
