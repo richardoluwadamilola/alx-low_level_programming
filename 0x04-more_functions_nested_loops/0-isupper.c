d0c1f9643d482509a6361c9a5bf34a701f97cfb7
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-07-07 03:28:55 -0700

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - a function that checks for uppercase
+ * @c: character to be checked
+ *
+ * Return: 1 if c is capital, otherwise 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ */
+int _putchar(char);
+int _isupper(int c);
