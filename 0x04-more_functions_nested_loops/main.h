98cba08092742a80ba721b67aed6d6eef923f0fb
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-07-07 05:14:55 -0700

    more numbers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,6 +6,7 @@
  * mul - multiply two numbers
  * print_void - prints numbers 0 - 9
  * print_most_numbers - prints 0 - 9 without 2 and 4
+ * more_numbers - print 0 - 14 ten times
  */
 int _putchar(char);
 int _isupper(int c);
@@ -13,3 +14,4 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
