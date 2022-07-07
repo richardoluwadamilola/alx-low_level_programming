ee625514aa6c6b9942938df7baa433ced7d22afa
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-07-07 04:48:30 -0700

    multiply

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,7 +3,9 @@
 /**
  * _putchar - writes the character c to stdout
  * _isdigit - checks if int is a digit between 0-9
+ * mul - multiply two numbers
  */
 int _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
