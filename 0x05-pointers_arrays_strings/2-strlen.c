e8e6cec5a89444d2446c683cf2641182fd9df8c0
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-07-12 02:14:50 -0700

    string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,9 +3,9 @@
 /**
  * _strlen - returns the length of the string
  * @s: input string to count
- * Return: length of sting as int
+ * Return: length  of string as int
  */
-int _strlen(char *s);
+int _strlen(char *s)
 {
 	int i = 0;
 
