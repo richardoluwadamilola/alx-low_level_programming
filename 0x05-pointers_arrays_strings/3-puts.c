522fd3859d5d7a8201b254c79976605cfcad6bca
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-07-12 02:25:02 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - printsstring
+ * @str: input string to print
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
