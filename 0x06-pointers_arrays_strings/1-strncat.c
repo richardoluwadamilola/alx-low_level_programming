f060f28176ec2591f8e6f8f718860deb2d102dc3
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-07-14 04:30:35 -0700

    fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,9 +2,9 @@
 
 /**
  * _strncat - concatenates specified values from src to dest
- * @src: second dtring to copy from
- * @dest: string to be overwritten
- * @n: number of values to concatenate
+ * @src: source
+ * @dest: destination
+ * @n: amount of bytes used from src
  * Return: pointer to dest
  */
 char *_strncat(char *dest, char *src, int n)
