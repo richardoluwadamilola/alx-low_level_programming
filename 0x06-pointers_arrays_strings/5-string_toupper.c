eeb81ca4a95b37acb2d2fc322aa7328eaa205dc9
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-07-14 03:31:07 -0700

    upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase to uppercase
+ *@str: string to modify
+ * Return: str
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+	}
+
+	return (str);
+}
