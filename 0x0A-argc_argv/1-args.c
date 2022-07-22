3981b2945cdfe1fa1120917c495e484d85dd6c87
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-07-22 04:04:09 -0700

    task2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the number of arguments entered
+ * @argc: number of arguments
+ * @argv: arguments received
+ * Return: zero
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	argv = argv;
+
+	return (0);
+}
