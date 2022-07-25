ca15be0db20d3292565a8d3618057710385cd35d
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-07-25 04:01:54 -0700

    done

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,1 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
 
+/**
+ * argstorstr - concatenates all the arguments of a program
+ * @ac: argument cout
+ * @av: argument vector
+ * Return: pointer of an array of char
+ */
+char *argstostr(int ac, char **av)
+{
+	char *aout;
+	int c, i, j, ia;
+
+	if (ac == 0)
+		return (NULL);
+
+	for (c = i = 0; i < ac; i++)
+	{
+		if (av[i] == NULL)
+			return (NULL);
+
+		for (j = 0; av[i][j] != '\0'; j++)
+			c++;
+		c++;
+	}
+	auot = malloc((c + 1) * sizeof(char));
+	if (auot == NULL)
+	{
+		free(auot);
+		return (NULL);
+	}
+	for (i = j = ia = 0; ia < c; j++, ia++)
+	{
+		if (av[i][j] == '\0')
+		{
+			auot[ia] = '\n';
+			i++;
+			ia++;
+			j = 0;
+		}
+		if (ia < c - 1)
+			auot[ia] = av[i][j];
+	}
+	auot[ia] = '\0';
+	return (auot);
+}
