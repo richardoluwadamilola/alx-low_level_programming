c461d5fe996a6caccbbdb10224700bf3f1e7a81d
RIchard Oluwadamilola Oluwatobilola <oluwatobyrichard@gmail.com>
2022-07-25 10:58:36 +0100

    fixed

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,21 +8,19 @@
  */
 char *_strdup(char *str)
 {
-	char *strout;
+char *strout;
 
-	unsigned int i, j;
+unsigned int i, j;
 
-	if (str == NULL)
-		return (NULL);
+if (str == NULL)
+return (NULL);
+for (i = 0; str[i] != '\0'; i++);
+strout = (char *)malloc(sizeof(char) * (i + 1));
+if (strout == NULL)
+return (NULL);
 
-	for (i = 0; str[i] != '\0'; i++);
+for (j = 0; j <= i; j++)
+strout[j] = str[j];
 
-	strout = (char *)malloc(sizeof(char) * (i + 1));
-	if (strout == NULL)
-		return (NULL);
-
-	for (j = 0; j <= i; j++)
-		strout[j] = str[j];
-
-	return (strout);
+return (strout);
 }
