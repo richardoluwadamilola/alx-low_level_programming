26cc3ad1ea3d394505bb765d9f419ae5f06016c8
RIchard Oluwadamilola Oluwatobilola <oluwatobyrichard@gmail.com>
2022-07-28 13:14:40 +0100

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,9 +9,9 @@ char *_memset(char *s, char b, unsigned int n);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
-void errors(void);
-void _is_zero(char *argv[]);
-char *_initialize_array(char *ar, int lar);
-int _checknum(char *argv[], int n);
-int main(int argc, char *argv[]);
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
 #endif
