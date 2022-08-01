65e090b1e2719ea23127a943b23ab45db62f425e
RIchard Oluwadamilola Oluwatobilola <oluwatobyrichard@gmail.com>
2022-08-01 11:41:17 +0100

    done

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-/*
- * main - print name of file compiled from
- * Return: Always 0
- */
+/**
+* main - prints the name of the file program was compiled from
+* Return: Always 0
+*/
 int main(void)
 {
-	printf("%s\n", __FILE__);
-	return (0);
+printf("%s\n", __FILE__);
+return (0);
 }
