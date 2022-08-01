86d1e0ff84aa33eb6a2e3cc65400dd75acc04b09
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-08-01 02:02:27 -0700

    task1

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Learnign structures typedef in C programming language
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: This struct is for dog
+ */
+struct dog
+{
+	char *nam;
+	e
