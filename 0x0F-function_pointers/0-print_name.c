e84417c90f99a3b448fc001f8b35e0ab5fc641df
RIchard Oluwadamilola Oluwatobilola <oluwatobyrichard@gmail.com>
2022-08-03 15:49:56 +0100

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,13 @@
 #include "function_pointers.h"
 
 /**
- * print_name - print name using pointer to function
- * @name: string to add
- * @f: pointer to function
- * Return: Nothing
- */
+* print_name - prints a name.
+* @name: input name.
+* @f: function pointer.
+* Return: no return.
+*/
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+if (name && f)
+f(name);
 }
