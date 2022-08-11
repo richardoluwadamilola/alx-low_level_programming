b486f28725c0a70c4f98fb5afcf7b4e87c6cff2a
richardoluwadamilola <oluwatobyrichard@gmail.com>
2022-08-11 02:26:58 -0700

    ündone

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * add_
+ * node - adds a new node at the beginning
+ * of a list_t list
+ * @head: head of the linked l
+ * ist
+ * @str: string to store in the list
+ * Return: address of the head */
+
