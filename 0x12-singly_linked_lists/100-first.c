6bb9c2b171e43644561f515f9e64b168c7bc2812
RIchard Oluwadamilola <oluwatobyrichard@gmail.com>
2022-08-11 10:42:42 +0100

    done

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,3 +9,5 @@ void tortoiseAndHare(void) __attribute__ ((constructor));
 void tortoiseAndHare(void)
 {
 	printf("You're beat! and yet, you must allow, \n");
+	printf("I bore my house upon my back!\n");
+}
