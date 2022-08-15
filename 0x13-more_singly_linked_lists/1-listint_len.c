816dbac5b985d739d13c0c4c0da744b3052da2a6
RIchard Oluwadamilola <oluwatobyrichard@gmail.com>
2022-08-15 11:11:24 +0100

    done

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns number of nodes in listint_t list
+ * @h: listint_t pointer
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t i;
+
+	if (h == NULL)
+		return (0);
+	for (i = 0; h != NULL; i++)
+		h = h->next;
+	return (i);
+}
