1828af3e8001d89d63c37968198ccb56667f4b83
RIchard Oluwadamilola <oluwatobyrichard@gmail.com>
2022-08-15 11:05:35 +0100

    done

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,22 @@
 #ifndef _MORE_SINGLY_LINKED_LISTS
 #define _MORE_SINGLY_LINKED_LISTS
 
-#include <stdlib.h>#include <string.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 
+/**
+*struct listint_s - singly linked list
+*@n: integer
+*@next: pointes to the next node
+*Description: singly linked list node structure
+*for ALX project
+*/
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
 
+size_t print_listint(const listint_t *h);
+#endif
