a3d77d273452bdcac42ba7cbf8d7dbcffe6d6920
RIchard Oluwadamilola <oluwatobyrichard@gmail.com>
2022-09-29 09:38:19 +0100

    Update 4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,16 +1,16 @@
 #include "lists.h"
 /**
-* free_dlistint - free a dlistint_t list
-* @head: head of list
-*/
+ * free_dlistint - free a dlistint_t list.
+ * @head: head of list
+ */
 void free_dlistint(dlistint_t *head)
 {
-  dlistint_t *kill;
-  
-  while (head)
-  {
-    kill = head;
-    head = head->next;
-    free(kill);
-   }
+	dlistint_t *kill;
+
+	while (head)
+	{
+		kill = head;
+		head = head->next;
+		free(kill);
+	}
 }
