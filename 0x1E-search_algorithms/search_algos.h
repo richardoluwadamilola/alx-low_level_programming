9fcd5b6d9cea096f282396584d90fb29ff877106
richardoluwadamilola <oluwatobyrichard@gmail.com>
2023-01-26 03:24:07 -0800

    Interpolation search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -44,5 +44,6 @@ void print_array(int *array, size_t l, size_t r);
 int binary_search_index(int *array, size_t l, size_t r, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif
